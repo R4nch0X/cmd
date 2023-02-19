d07788906829d50625cb13001dc18746947607dd
Richard Andrés Echavalete <ranchox@gmail.com>
2023-02-19 04:13:53 -0300

    config donctrine actualizaciones y limpieza

--- app/Http/Api/Handler/Team/TeamCreateHandler.php
+++ app/Http/Api/Handler/Team/TeamCreateHandler.php
@@ -1,34 +1,41 @@
 <?php
 
-namespace App\Http\Api\Handler;
+namespace App\Http\Api\Handler\Team;
 
+use App\Http\Api\Handler\Handler;
 use App\Http\Api\Requests\TeamCreateRequest;
 use App\Http\Kernel;
-
 use Cmd\Services\Teams;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Router;
 
-#use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+//use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+//use App\Http\Utils\Handlers;
+
+//use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class TeamCreateHandler extends Handler
 {
-
     public function __invoke(TeamCreateRequest $teamCreateRequest, Teams $team): JsonResponse
     {
-        $teamCreateRequest->validate();
+        $teamCreateRequest->validation();
         $team->create();
 
         return responder()->success()->respond(Response::HTTP_CREATED);
     }
 
-
-    public static function defineRoute(Router $router):void
+    public static function defineRoute(Router $router): void
     {
         $router->post('team', self::class)
             ->prefix('api')
             ->name(self::class)
             ->middleware([Kernel::API]);
+    }
+
+    public static function routePriority(Router $route): int
+    {
+        return 1;
     }
 }
